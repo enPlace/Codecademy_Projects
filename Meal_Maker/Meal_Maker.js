d09290de2af5d8a943bf7db30e702aa0ab4a7e98
enPlace <n.place@protonmail.com>
2021-03-04 20:38:42 -0300

    getters and setters

--- Meal_Maker/Meal_Maker.js
+++ Meal_Maker/Meal_Maker.js
@@ -13,10 +13,22 @@ const menu = {
   }, 
   get desserts(){
     return this._courses.desserts
+  }, 
+  get courses(){
+    return this._courses
+  }, 
+  set appetizers (appetizers){
+    this._courses.appetizers = appetizers
+  }, 
+  set mains (mains){
+    this._courses.mains = mains
+  }, 
+  set desserts (dessserts){
+    this._courses.desserts = desserts
   }
 }
 
 
 
-console.log(menu.desserts)
+console.log(menu.courses)
 
