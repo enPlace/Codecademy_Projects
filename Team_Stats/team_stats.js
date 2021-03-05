eb34719f1642c0bb720b3b08735a620bedc1f1aa
enPlace <n.place@protonmail.com>
2021-03-05 15:37:32 -0300

    addPlayer method

--- Team_Stats/team_stats.js
+++ Team_Stats/team_stats.js
@@ -19,25 +19,24 @@ const team = {
 		}, 
 	], 
 
-	_games: {
-		game1:{
+	_games: [{
 			opponent: 'Milwaukee Bucks',
 			opponentPoints: 112, 
 			teamPoints: 111, 
 
 		}, 
-		game2: {
+		{
 			opponent: 'Washington Wizards', 
 			opponentPoints: 111,
 			teamPoints: 125
 		}, 
-		game3: {
+		{
 			opponent: 'Houston Rockets', 
 			opponentPoints: 84, 
 			teamPoints: 133
 		}
+	],
 
-	}, 
 	get games(){
 		return this._games
 	}, 
@@ -51,9 +50,17 @@ const team = {
 			age: age
 		}; 
 		return this._players.push(player)
+	}, 
+	addGame (opponent, opponentPoints, teamPoints) {
+		const game = {
+			opponent: opponent, 
+			opponentPoints: opponentPoints, 
+			teamPoints: teamPoints
+		};
+		return this._games.push(game)
 	}
 }
-
+team.addGame ('Los Angeles Clippers', 119, 99)
 console.log(team.games)
 team.addPlayer('Kyle', 'Anderson', 27)
 console.log(team.players)
