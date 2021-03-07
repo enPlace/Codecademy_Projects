add82b462980a2fbb72a18bab52f66f96f650efc
enPlace <n.place@protonmail.com>
2021-03-07 14:56:52 -0300

    updating practice

--- practice.js
+++ practice.js
@@ -4,16 +4,25 @@ class Surgeon {
     this._department = department;
     this._remainingVacationDays = 20;
   }
-  get name ( ){
-    return this._name
+  
+  get name() {
+    return this._name;
   }
-  get department () {
-    return this._department
+  
+  get department() {
+    return this._department;
   }
-  get remainingVacationDays (){
-    return this._remainingVacationDays
+  
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+  
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
   }
 }
 
 const surgeonRomero = new Surgeon('Francisco Romero', 'Cardiovascular');
 const surgeonJackson = new Surgeon('Ruth Jackson', 'Orthopedics');
+console.log(surgeonRomero.takeVacationDays(3))
+console.log(surgeonRomero.remainingVacationDays)
