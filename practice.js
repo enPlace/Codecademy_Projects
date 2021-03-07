bd004127b786e4ff8f16cf9606971012ff71f780
enPlace <n.place@protonmail.com>
2021-03-07 14:41:30 -0300

    updating practice

---
+++ practice.js
@@ -0,0 +1,19 @@
+class Surgeon {
+  constructor(name, department) {
+    this._name = name;
+    this._department = department;
+    this._remainingVacationDays = 20;
+  }
+  get name ( ){
+    return this._name
+  }
+  get department () {
+    return this._department
+  }
+  get remainingVacationDays (){
+    return this._remainingVacationDays
+  }
+}
+
+const surgeonRomero = new Surgeon('Francisco Romero', 'Cardiovascular');
+const surgeonJackson = new Surgeon('Ruth Jackson', 'Orthopedics');
