91e32f545a8cb366d0734207aeeb7e4bffcd00be
enPlace <n.place@protonmail.com>
2021-03-09 12:14:16 -0300

    book and movie instances

--- Build_A_Library/build_a_library.js
+++ Build_A_Library/build_a_library.js
@@ -43,3 +43,46 @@ class Book extends Media{
 		return this._pages
 	}
 }
+
+
+class Movie extends Media{
+	constructor(director, title, runTime){
+		super(title); 
+		this._director = director; 
+		this._runTime = runTime
+	}
+	get director(){
+		return this._director
+	}
+	get runTime(){
+		return this.runTime
+	}
+}
+
+class CD extends Media{
+	constructor(artist, title){
+		super(title); 
+		this._artist = artist; 
+		
+	}
+
+}
+
+const historyOfEverything = new Book('Bill Bryson', 'A Short History of Nearly Everything', 
+	544)
+
+historyOfEverything.toggleCheckedOutStatus()
+console.log(historyOfEverything.isCheckedOut)
+historyOfEverything.addRating(4)
+historyOfEverything.addRating(5)
+historyOfEverything.addRating(5)
+console.log(historyOfEverything.getAverageRating())
+
+
+const speed = new Movie('Jan de Bont', 'Speed', 116)
+speed.toggleCheckedOutStatus()
+console.log(speed.isCheckedOut)
+speed.addRating(1)
+speed.addRating(1)
+speed.addRating(5)
+console.log(speed.getAverageRating())
