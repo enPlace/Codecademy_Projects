ccb0456c8371fb0a4a104feafe953382863c71e4
enPlace <n.place@protonmail.com>
2021-03-10 14:26:28 -0300

    adding subclasses

--- School_Catalogue/school_catalogue.js
+++ School_Catalogue/school_catalogue.js
@@ -22,6 +22,52 @@ class School {
 	get numberOfStudents(){
 		return this._numberOfStudents
 	}
+	set level(value){
+		if (value !== 'primary'&& value !== 'middle' && value !== 'high'){
+			console.log("The school level input must be either 'primary', 'middle', or 'high'.") 
+	}else{
+		this._level = value
+	}
+	}
+
+	set numberOfStudents(value){
+				if (typeof value != 'number'){
+			console.log("The input for number of students must be a number.")
+		}
+		else if (typeof value=== 'number' && value %1 != 0){
+			console.log("The input for the number of students must be a whole number.")
+
+	}else {
+		this._numberOfStudents = value
+	}
+	}
+
+}
+
+
+class Primary extends School{
+	constructor(name, level, numberOfStudents)
+	super(name);
+	super(level);
+	super(numberOfStudents)
+}
+class Middle extends School{
+	constructor(name, level, numberOfStudents)
+	super(name);
+	super(level);
+	super(numberOfStudents)
+}
+class High extends School{
+	constructor(name, level, numberOfStudents)
+	super(name);
+	super(level);
+	super(numberOfStudents); 
+	this._sportsTeams = []
 
 }
 const newschool = new School('Ham', 'primary', 1)
+console.log(newschool.level)
+newschool.level = 'middle'
+console.log(newschool.level)
+newschool.numberOfStudents = 'test'
+console.log(newschool.numberOfStudents)
