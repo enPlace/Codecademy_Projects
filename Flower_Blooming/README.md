b6d8b7447991cb925ea64dd1ca050b7808880052
enPlace <n.place@protonmail.com>
2021-03-22 15:13:37 -0300

    Initial Commit

---
+++ Flower_Blooming/README.md
@@ -0,0 +1,3 @@
+#Flower_Blooming
+
+Upon clicking on the logo, the website background will change. 
--- practice.js
+++ practice.js
@@ -1,51 +1,5 @@
-class School {
-	constructor(name, level, numberOfStudents){
-		this._name = name; 
-		this._level = level; 
-		this._numberOfStudents = numberOfStudents
-
-	}
-	get name(){
-		return this._name
-	}
-	get level(){
-		return this._level
-	}
-	get numberOfStudents(){
-		return this._numberOfStudents
-	}
-	set level(value){
-		this._level = value
-	}
-	set numberOfStudents(value){
-		this._numberOfStudents = value
-	}
-	quickFacts(){
-		console.log(`${this.name} educates ${this.numberOfStudents} students at the ${this.level} school level`)
-	}
-
-};
-
-
-class Primary extends School{
-	constructor(name, level, numberOfStudents){
-	super(name, level, numberOfStudents);}
-}
-class Middle extends School{
-	constructor(name, level, numberOfStudents){
-	super(name, level, numberOfStudents);}
-}
-class High extends School{
-	constructor(name, level, numberOfStudents){
-	super(name, level, numberOfStudents); 
-	this._sportsTeams = []
-	}
-
-}
-const newschool = new Primary('Ham', 'primary', 1)
-console.log(newschool.level)
-newschool.level = 'middle'
-console.log(newschool.level)
-newschool.numberOfStudents = 'test'
-console.log(newschool.numberOfStudents)
-newschool.quickFacts()
+var str = "Visit Microsoft Microsoft!";
+var regex = /m/gi;
+console.log(str.replace(regex, "B"))
+console.log([ 'Pippi', 'Was', 'Pippi' ].join(""))
+console.log(str.slice(2,4))
